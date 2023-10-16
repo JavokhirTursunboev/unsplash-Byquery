5b0e872ab2a0692762239a484ac8ea2bd59485fb
Javokhir <jtursunboev@bk.ru>
2023-10-16 19:01:31 +0900

    added search info

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title">UNsplash Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          className=" form-input search-input"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
-
 export default SearchForm;

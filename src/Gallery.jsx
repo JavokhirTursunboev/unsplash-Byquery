c2eb089ba6d32980a8f332f3b6a8f53ec47b4467
Javokhir <jtursunboev@bk.ru>
2023-10-20 19:31:35 +0900

    give and take response url

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -11,8 +11,49 @@ const Gallery = () => {
       return result;
     },
   });
-  console.log(response);
-  return <div>Gallery</div>;
+
+  // ============ Loading ===================//
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  // ========= Error ==================//
+  if (response.error) {
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+  // =============== Not existing search ====================//
+  const results = response.data.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>Not result found...</h4>
+      </section>
+    );
+  }
+  console.log(results);
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item.urls.regular;
+        return (
+          <img
+            src={url}
+            alt={item.alt_description}
+            key={item.id}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
 };
 
 export default Gallery;

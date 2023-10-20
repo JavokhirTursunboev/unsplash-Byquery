4bb78001bbc4d7c07eaa80d7b5997745b56130f7
Javokhir <jtursunboev@bk.ru>
2023-10-20 19:37:13 +0900

    installed reactquerydevtools

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
 const url =
-  "https://api.unsplash.com/search/photos?client_id=5tYOQSMJU4ViQEf2_O7a2_syq1NppI7j_isSxBVpFAo&query=office";
+  "https://api.unsplash.com/search/photos?client_id=5tYOQSMJU4ViQEf2_O7a2_syq1NppI7j_isSxBVpFAo&query=horse";
 const Gallery = () => {
   const response = useQuery({
     queryKey: ["images"],
